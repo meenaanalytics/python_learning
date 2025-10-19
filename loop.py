247b68b3fd3dcf2e1c347022479fb101863be075
Guggu kaka <vishalkumar@Guggus-MacBook-Air.local>
2025-10-19 17:16:27 +0530

    first changes by guggu❤️

---
+++ loop.py
@@ -0,0 +1,64 @@
+# for vishal in range(5):
+#     print("Hello", vishal)
+
+
+
+# for k in range(20):
+#     print(k)
+
+
+
+# fruits = ["apple", "banana", "orange"]
+
+# for falll in fruits:
+#     print('outside if condition : ', falll)
+#     if(falll == 'orange'):
+#         print(falll)
+
+
+
+# for i in range(100):
+#     print(i)
+# else:
+#     print('numbers printed succesfully!')
+
+
+
+# for student in students:
+#     if student == 'Vishal':
+#         print('Sorry not allowed!!!!', student)
+#         break
+
+#     print('Welcome to sports room!!', student)
+    
+# students = ['Ram', 'Sham', "ghanshyam", 'Raghu', 'Vishal', 'Sourav' , 'Gouraav', 'Suraj']
+# index = 0
+# current_student = students[index]
+
+# while current_student != 'Vishal':
+#     print('Welcome to sports room >', current_student)
+#     index = index + 1
+#     current_student = students[index]   # move to next student
+
+# print('Found Vishal! Ending loop.')
+
+
+
+
+# password = ""
+# while password != "python123":
+#     password = input("Enter password: ")
+
+# print("Access granted ✅")
+
+
+
+
+# password = ""
+# for attempt in range(3):
+#     password = input("Enter password: ")
+#     if password == "python123":
+#         print("Welcome")
+#         break
+# else:
+#     print("Too many attempts!")
---
+++ loop_practise.py
@@ -0,0 +1,42 @@
+# 🏋️ 1. For Loop
+# Write a for loop to print numbers from 1 to 10.
+# 📝 Hint: Use range(1, 11).
+
+# for i in range(1 , 11):
+#     print(i)
+
+
+# 🧮 2. While Loop
+# Keep asking the user to enter their favorite number until they enter 0.
+# When they enter 0, print "Goodbye!".
+# a = 4
+# while a != 0:
+#     a =int(input("enter the number: "))
+    
+# print("good bye" , a)
+
+
+# 🍎 3. For Loop with a List
+# Create a list of fruits (apple, banana, mango, orange)
+# Use a for loop to print:
+
+# fruits=["mango" , "grapes" , "papaya" , "apple" , "pineapple" , "orange"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+
+
+# 🕹️ 4. While Loop with Condition
+# Start with a variable count = 5.
+# Use a while loop to count down to 1, then print “🚀 Lift Off!”
+# 📝 Hint: count -= 1 will help you reduce the number each loop.
+
+# count = 5
+# while count !=1:
+#     print(count)
+#     count= count -1
+
+# print("lift off")

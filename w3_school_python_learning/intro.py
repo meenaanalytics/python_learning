990ab860b6dc7f766595c6e0a90ae1c6c527f7d7
Guggu kaka <vishalkumar@Guggus-MacBook-Air.local>
2025-10-23 10:09:59 +0530

    started w3school python learning journey

---
+++ w3_school_python_learning/intro.py
@@ -0,0 +1,78 @@
+# print('Hello world')
+
+# while True:
+#     print('hello')
+
+# print('bye')
+
+# import sys
+
+# print(sys.version)
+
+'''identation'''
+
+# if 5 > 2:
+#     print('5 is greater then 2')
+#               print('done with the calculation')
+
+'''Variables'''
+
+# x = 5
+# y = 'Hello World'
+
+# realted to president security
+# print('very secret mission')
+
+
+# x = 'Vishal'
+# x = 'Ram'
+# x = 'Vishal'
+# X = 'Sham'
+
+# x = 5
+# x = 'Car'
+# x = 5.342323
+# x = ['hi','bye']
+
+# var = str(3)
+# num = int('230498230')
+# flat_val = float('5')
+
+# print(type(var))
+# print(type(num))
+# print(type(flat_val))
+
+'''variable creation rules'''
+
+# _brand = 'Audi'
+# brand = 'Audi'
+
+# brand8_color = 'Red'
+# BRAND8_COLOR = 'green'
+#7brand = 'Audi'.  # ❌ wrong
+
+# print_print = 'vishal'
+
+# sahil_class_10th_marks = 80
+# kondapur_bike_showroom_count = 70 #snake case
+KondapurBikeShowroomCount = 70  #pascal case / camel case
+
+print(KondapurBikeShowroomCount)
+
+
+
+'''quotes'''
+# print("This will cause an error")
+
+'''print without new line'''
+
+# print("Hello World!", end="******")
+# print("I will print on the same line.")
+
+'''print python numbers'''
+
+# print(3)
+# print(56)
+# print(45+55)
+
+# print('Hi my age is', 35, 'years old', 56, 'sadflksjdf', 45+ 789, 'sdlfkasdlf')
